d42b3b552669fc44fccab5e7ee5067e4807f31a9
Stephane Wirtel <stephane@openerp.com>
2010-09-03 18:23:15 +0200

    [RELEASE] OpenERP 5.0.14

--- bin/release.py
+++ bin/release.py
@@ -22,7 +22,7 @@
 ##############################################################################
 
 name = 'openerp-client'
-version = '5.0.13'
+version = '5.0.14'
 description = 'OpenERP Client'
 long_desc = '''\
 OpenERP is a complete ERP and CRM. The main features are accounting (analytic
