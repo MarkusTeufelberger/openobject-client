7ee077c3ede8e37cda25b919f2dd561344fd3be8
gpa (Open ERP) <gpa@tinyerp.com>
2012-06-15 12:33:52 +0530

    [FIX] from calendar view create customer or supplier invoice when select partner it generate error: (Maintenance Case : 575841)

--- bin/widget/view/calendar_gtk/parser.py
+++ bin/widget/view/calendar_gtk/parser.py
@@ -242,7 +242,10 @@ class ViewCalendar(object):
                 # simply display new current day
                 return
             elif hippo_event.count >= 2: # double clic or more
-                self.screen.context.update({'default_' +self.date_start:self.date.strftime('%Y-%m-%d %H:%M:%S')})
+                val = self.date.strftime('%Y-%m-%d %H:%M:%S')
+                if self.fields[self.date_start]['type'] == 'date':
+                    val = self.date.strftime('%Y-%m-%d')
+                self.screen.context.update({'default_' +self.date_start:val})
                 self.screen.switch_view(mode='form')
                 self.screen.new()
 
